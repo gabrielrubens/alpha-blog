86ff347ad1e8aaf77e3dddb9ab8a0c880ea0c07b
Gabriel Rubens <gabrielrubens@gmail.com>
2020-05-10 10:30:25 +0000

    Add delete user and dependent articles destroy functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,14 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+    
+  end
+  
   private
   
   def set_user
